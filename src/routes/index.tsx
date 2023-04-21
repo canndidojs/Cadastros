63c5b2177f546a9f3ebaae078b25b384b924dece
Fabricio Candido <cf.pessoal13@gmail.com>
2023-04-21 12:17:47 -0300

    Adicionado routerdom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+            /** Não cair para tela em branco sem sentido, sempre buscar a página inicial caso for digitado algo diferente na url. */
+
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Routes>
+    );
+}
