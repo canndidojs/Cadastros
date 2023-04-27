f277cba670c81b079e469502a908639fec264024
Fabricio Candido <cf.pessoal13@gmail.com>
2023-04-27 13:47:24 -0300

    feat: Adicionado listagem de cidades

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export * from './dashboard/Dashborad';
+export * from './cidades/ListagemDeCidade'
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,40 @@
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
-import { Dashboard } from '../pages';
+import { useDrawerContext } from '../shared/contexts';
+import {
+  Dashboard,
+  ListagemDeCidade
+
+} from '../pages';
 
 export const AppRoutes = () => {
-    const { setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
-   
-    useEffect(() => {
-        setDrawerOptions([
-          {
-            icon: 'home',
-            path: '/pagina-inicial',
-            label: 'Página inicial',
-          }
-        ]);
-      }, []);
 
-    return (
-        <Routes>
-            <Route path="/pagina-inicial" element={<Dashboard />} />
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',
+      },
+      {
+        icon: 'apartment',
+        path: '/cidades',
+        label: 'Cidades',
+      },
+    ]);
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Dashboard />} />
             /** Não cair para tela em branco sem sentido, sempre buscar a página inicial caso for digitado algo diferente na url. */
 
-            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
-        </Routes>
-    );
+      <Route path="/cidades" element={<ListagemDeCidade />} />
+      {/*<Route path="/cidades/detalhe/:id" element={<Dashboard />} />*/}
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
 }
