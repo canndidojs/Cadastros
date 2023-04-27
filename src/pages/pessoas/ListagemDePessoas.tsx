357c32569aa1c3cb88d8261a17e87540c6da052f
Fabricio Candido <cf.pessoal13@gmail.com>
2023-04-27 18:17:56 -0300

    feat: Adicionado paginação

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,9 +4,10 @@ import { useSearchParams } from "react-router-dom";
 import { PessoasService, IListagemPessoa } from "../../shared/services/api/pessoas/PessoasService";
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { Environment } from "../../shared/environment";
 import { useDebounce } from "../../shared/hooks";
 
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableFooter, LinearProgress, Pagination } from "@mui/material";
 
 export const ListagemDePessoas: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -16,32 +17,33 @@ export const ListagemDePessoas: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [totalCount, setTotalCount] = useState(0);
 
-
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
-
-    useEffect(() => {
-        setIsLoading(true);
-
-        debounce(() => {
-            PessoasService.getAll(1, busca)
-                .then((result) => {
-                    setIsLoading(false);
-
-                    if (result instanceof Error) {
-                        alert(result.message)
-                    } else {
-                        console.log(result)
-
-                        setRows(result.data);
-                        setTotalCount(result.totalCount);
-                    }
-                })
-        });
-    }, [busca])
-
+    const pagina = useMemo(() => {
+        return Number(searchParams.get('pagina') || '1');
+    }, [searchParams])
+    
+        useEffect(() => {
+            setIsLoading(true);
+    
+            debounce(() => {
+                PessoasService.getAll(pagina, busca)
+                    .then((result) => {
+                        setIsLoading(false);
+    
+                        if (result instanceof Error) {
+                            alert(result.message)
+                        } else {
+                            console.log(result)
+    
+                            setTotalCount(result.totalCount);
+                            setRows(result.data);
+                        }
+                    });
+            });
+        }, [pagina, busca ])
 
     return (
         <LayoutBaseDePagina
@@ -51,7 +53,7 @@ export const ListagemDePessoas: React.FC = () => {
                     mostrarInputBusca
                     textoDaBusca={busca}
                     textoBotaoNovo='Nova'
-                    aoMudarTextoDeBusca={texto => console.log(setSearchParams({ busca: texto }, { replace: true }))}
+                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto, pagina: '1' }, { replace: true })}
                 />
             }
         >
@@ -66,18 +68,48 @@ export const ListagemDePessoas: React.FC = () => {
                     </TableHead>
 
                     <TableBody>
-
                         {rows.map(row => (
                             <TableRow key={row.id}>
                                 <TableCell>Ações</TableCell>
                                 <TableCell>{row.nomeCompleto}</TableCell>
                                 <TableCell>{row.email}</TableCell>
                             </TableRow>
-
                         ))}
                     </TableBody>
+
+                    {totalCount === 0 && !isLoading && (
+                        <caption>{Environment.LISTAGEM_VAZIA}</caption>
+                    )}
+
+                    <TableFooter>
+                        {isLoading && (
+                            <TableRow>
+                                <TableCell colSpan={3} sx={{ p: 0 }}>
+
+                                    <LinearProgress variant='indeterminate' />
+                                </TableCell>
+                            </TableRow>
+                        )}
+
+
+                        {(totalCount > Environment.LIMITE_DE_LINHAS) && (
+                            <TableRow>
+                                <TableCell colSpan={3} sx={{ p: 0 }}>
+                                    <Pagination
+                                        page={pagina}
+                                        count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                                        onChange={(e, newPage) => setSearchParams({pagina: newPage.toString(), busca })}
+
+
+                                        shape="rounded"
+                                        sx={{ p: 1 }}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableFooter>
                 </Table>
             </TableContainer>
-        </LayoutBaseDePagina>
+        </LayoutBaseDePagina >
     );
 };
